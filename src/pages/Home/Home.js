247f4749416f13132993299203438eea2ba9b252
Joao Pedro Mairinque <jpmairinque@gmail.com>
2021-12-01 20:32:31 -0300

    test commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setShips } from '../../redux/actions/ShipsActions'
 import { ShipsList } from '../../components/ShipsList/ShipsList'
